87467294d033dc8d3cbd787970786453efb01028
Rakul Inthrakumar <rakul.inthrakumar@fraktal.no>
2025-11-05 09:48:21 +0000

    new tables

---
+++ models/intermediate/eksfin/silver_projects_transactions.sql
@@ -0,0 +1,17 @@
+with projects as (
+    select * from {{ ref('stg_projects') }}
+),
+link as (
+    select * from {{ ref('stg_link') }}
+),
+transformed as (
+    select * 
+    from 
+        projects p
+    inner join 
+        link l
+    on
+        p.project_id =  l.project_id
+
+)
+select * from transformed
